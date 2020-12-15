5d4741c9b448d7ea4bc6691c0831a0176b7494f2
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-12-15 13:46:14 -0600

    updated syntax of class to element

--- project1/jsx/src/index.js
+++ project1/jsx/src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom'
 const App = () => {
     return (
     <div>
-        <label class ="label" for="name">Enter name:</label>
+        <label className="label" for="name">Enter name:</label>
         <input id="name" type="text" />
         <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
     </div>
