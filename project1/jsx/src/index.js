015ef506132964b502e7150d0b415f5238644485
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-12-15 13:36:19 -0600

    converted HTML to JSX with inline styling w/ JSX

--- project1/jsx/src/index.js
+++ project1/jsx/src/index.js
@@ -4,8 +4,14 @@ import ReactDOM from 'react-dom'
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>
-}
+    return (
+    <div>
+        <label class ="label" for="name">Enter name:</label>
+        <input id="name" type="text" />
+        <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+    </div>
+    );
+};
 
 // Take the React component and show it on the screen
 ReactDOM.render(
