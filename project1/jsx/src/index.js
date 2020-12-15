17f2090d0fbdc17118bf948a5c33266f315139bf
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-12-15 14:03:53 -0600

    created variables for text displayed, updated DOM property of for

--- project1/jsx/src/index.js
+++ project1/jsx/src/index.js
@@ -2,21 +2,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function getButtonText() {
-    return 'Click on me!';
-}
-
 // Create a react component
 const App = () => {
-    // const buttonText = 'Click Me!'
+    const buttonText = { text:'Click Me!'}
+    const labelText = 'Enter name:'
 
     return (
     <div>
-        <label className="label" for="name">Enter name:</label>
+        <label className="label" htmlFor="name">{labelText}</label>
         <input id="name" type="text" />
         <button style={{backgroundColor: 'blue', color: 'white'}}>
-            {getButtonText()}
-            {/* {buttonText} */}
+            {buttonText.text}
         </button>
     </div>
     );
