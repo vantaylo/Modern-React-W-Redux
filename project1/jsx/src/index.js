32403ec089050ae195c701466981d3784ad9c3d5
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-12-15 13:51:54 -0600

    referenced JS variable in JSX

--- project1/jsx/src/index.js
+++ project1/jsx/src/index.js
@@ -2,13 +2,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function getButtonText() {
+    return 'Click on me!';
+}
+
 // Create a react component
 const App = () => {
+    // const buttonText = 'Click Me!'
+
     return (
     <div>
         <label className="label" for="name">Enter name:</label>
         <input id="name" type="text" />
-        <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        <button style={{backgroundColor: 'blue', color: 'white'}}>
+            {getButtonText()}
+            {/* {buttonText} */}
+        </button>
     </div>
     );
 };
