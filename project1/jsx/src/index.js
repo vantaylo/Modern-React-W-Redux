ce676d7db35995505f06afe498fbeb08909beb53
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-12-15 12:07:25 -0600

    created react component and displayed it

--- project1/jsx/src/index.js
+++ project1/jsx/src/index.js
@@ -3,7 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // Create a react component
-
-
+const App = () => {
+    return <div>Hi there!</div>
+}
 
 // Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
