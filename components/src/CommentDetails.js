e715909f334b52ed2f24890881aaaeebf9e4a040
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2021-01-05 16:09:55 -0600

    passing and receiving a prop

--- components/src/CommentDetails.js
+++ components/src/CommentDetails.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+//console.log("My props", props)
+
     return (
         <div className="comment">
                 <a href="/" className="avatar">
                     <img alt="avatar" src='https://source.unsplash.com/random' />
                 </a>
                 <div className="content">
-                    <a href="/" className="author">Sam3</a>
+                    <a href="/" className="author">{props.author}</a>
                     <div className="metadata">
                         <span className="date">Today at 6:00PM</span>
                     </div>
--- components/src/index.js
+++ components/src/index.js
@@ -5,7 +5,9 @@ import CommentDetail from "./CommentDetails";
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail/>
+            <CommentDetail author="Alex"/>
+            <CommentDetail author="Jane"/>
+            <CommentDetail author="Sam"/>
         </div>
     );
 };
