687fc32aa20b126117ba7a691fcadc16dee92587
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2021-01-05 14:58:30 -0600

    extracted JSX into new component

---
+++ components/src/CommentDetails.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CommentDetail = () => {
+    return (
+        <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src='https://source.unsplash.com/random' />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">Sam3</a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice Blog Post!</div>
+                </div>
+            </div>
+    )
+}
