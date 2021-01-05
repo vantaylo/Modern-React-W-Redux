e05fe3b25824a1b671fda329cefcc73d6f39c202
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2021-01-05 16:32:56 -0600

    passing and receiving multiple props

--- components/src/CommentDetails.js
+++ components/src/CommentDetails.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const CommentDetail = (props) => {
-//console.log("My props", props)
+console.log("My props", props)
 
     return (
         <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" src='https://source.unsplash.com/random' />
+                    <img alt="avatar" src={props.profilePic} />
                 </a>
                 <div className="content">
                     <a href="/" className="author">{props.author}</a>
                     <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
+                        <span className="date">{props.timeAgo}</span>
                     </div>
-                    <div className="text">Nice Blog Post!</div>
+                    <div className="text">{props.comment}</div>
                 </div>
             </div>
     )
--- components/src/index.js
+++ components/src/index.js
@@ -5,9 +5,9 @@ import CommentDetail from "./CommentDetails";
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Alex"/>
-            <CommentDetail author="Jane"/>
-            <CommentDetail author="Sam"/>
+            <CommentDetail author="Alex" date="yesterday" comment="Nice Blog Post!" profilePic="https://source.unsplash.com/random" />
+            <CommentDetail author="Jane" date="6 hours ago" comment="Cool!" profilePic="https://source.unsplash.com/random"/>
+            <CommentDetail author="Matt" date="1 hour ago" comment="Interesting!" profilePic="https://source.unsplash.com/random"/>
         </div>
     );
 };
