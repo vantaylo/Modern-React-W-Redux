19420250765b6ca0224342a11c2946f32972eaf5
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2021-01-04 18:55:47 -0600

    Duplicate a single component

--- components/src/index.js
+++ components/src/index.js
@@ -9,13 +9,40 @@ const App = () => {
                     <img alt="avatar" src='https://source.unsplash.com/random' />
                 </a>
                 <div className="content">
-                    <a href="/" className="author">Sam</a>
+                    <a href="/" className="author">Sam1</a>
                     <div className="metadata">
                         <span className="date">Today at 6:00PM</span>
                     </div>
                     <div className="text">Nice Blog Post!</div>
                 </div>
             </div>
+
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src='https://source.unsplash.com/random' />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">Sam2</a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice Blog Post!</div>
+                </div>
+            </div>
+
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src='https://source.unsplash.com/random' />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">Sam3</a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice Blog Post!</div>
+                </div>
+            </div>
+
         </div>
 
     );
